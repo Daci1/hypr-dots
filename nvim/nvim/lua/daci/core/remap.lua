c3766a7e1e521df2ffe10fccfb7dfd1917c7d573
Daci1 <dacian.barbu28@gmail.com>
2024-08-08 22:39:17 +0300

    Finally adds copy/paste shortcut for nvim

--- nvim/nvim/lua/daci/core/remap.lua
+++ nvim/nvim/lua/daci/core/remap.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>p", 'o<ESC>"+P', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>p", 'o<ESC>"+p', { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })
