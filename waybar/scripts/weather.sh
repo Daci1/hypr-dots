4ce2a94f60b071c1e8a3afb89777c19cda3a3eec
Daci1 <dacian.barbu28@gmail.com>
2025-01-18 17:06:00 +0200

    Waybar changes

--- waybar/scripts/weather.sh
+++ waybar/scripts/weather.sh
@@ -4,7 +4,7 @@ LOC="$1"
 # HTML encode string as %20
 LOCATION=$(sed -e "s/ /%20/g" <<<"$LOC")
 content=$(curl -sS "https://thisdavej.azurewebsites.net/api/weather/current?loc=$LOCATION&deg=C")
-ICON=$(curl -s 'https://wttr.in/?format=1' | sed 's/[+0-9a-cA-Z°-]//g' )
+ICON=$(curl -s "https://wttr.in/$1?format=1" | sed 's/[+0-9a-cA-Z°-]//g' )
 # echo $ICON
 TEMP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType)"' | sed 's/"//g')
 TOOLTIP=$(echo $content | jq -r '. | "\(.temperature)°\(.degType)\n\(.skytext)"' | sed 's/"//g')
